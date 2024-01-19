1bd42fc0d6cf391d940e178b1c0bd31f617445c5
UdayKandalkar <udaybhaukandalkar@gmail.com>
2024-01-19 08:26:13 +0530

    code

---
+++ src/test/java/com/cicdAssignment/CtsCicdAssignment/CtsCicdAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cicdAssignment.CtsCicdAssignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtsCicdAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
